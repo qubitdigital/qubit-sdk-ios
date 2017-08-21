b93bd6ed086e78fe3791de2c8441a5e26bcff9a7
Pavlo Davydiuk <pavlo.davydiuk@miquido.com>
2017-08-21 14:37:01 +0200

    Add skeleton for API requests

---
+++ QBTracker/QBTracker/APIClient/Entities/ConfigurationEntity.swift
@@ -0,0 +1,29 @@
+//
+//  ConfigurationEntity.swift
+//  QBTracker
+//
+//  Created by Pavlo Davydiuk on 21/08/2017.
+//  Copyright © 2017 Qubit. All rights reserved.
+//
+
+import Foundation
+
+struct ConfigEntity: Codable {
+    let sendAutoViewEvents: Bool
+    let sendAutoInteractionEvents: Bool
+    let sendGeoData: Bool
+    let disabled: Bool
+    let configurationReloadInterval: Int
+    let queueTimeout: Int
+    let vertical: String
+    
+    private enum CodingKeys : String, CodingKey {
+        case sendAutoViewEvents = "send_auto_view_events"
+        case sendAutoInteractionEvents = "send_auto_interaction_events"
+        case sendGeoData = "send_geo_data"
+        case disabled
+        case configurationReloadInterval = "configuration_reload_interval"
+        case queueTimeout = "queue_timeout"
+        case vertical
+    }
+}
