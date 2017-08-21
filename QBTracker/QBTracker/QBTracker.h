522741de3b0d5de7c54c9a7b8840b0194900c429
Pavlo Davydiuk <pavlo.davydiuk@miquido.com>
2017-08-21 12:39:36 +0200

    Initial commit

---
+++ QBTracker/QBTracker/QBTracker.h
@@ -0,0 +1,19 @@
+//
+//  QBTracker.h
+//  QBTracker
+//
+//  Created by Pavlo Davydiuk on 21/08/2017.
+//  Copyright © 2017 Qubit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QBTracker.
+FOUNDATION_EXPORT double QBTrackerVersionNumber;
+
+//! Project version string for QBTracker.
+FOUNDATION_EXPORT const unsigned char QBTrackerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QBTracker/PublicHeader.h>
+
+
