54919ea289d0b20956001ecc1050bdd152e729f8
Pavlo Davydiuk <pavlo.davydiuk@miquido.com>
2017-08-28 14:59:26 +0200

    Add lookup manager to initalization

--- QBTracker/QBTracker/QBTracker.swift
+++ QBTracker/QBTracker/QBTracker.swift
@@ -15,6 +15,7 @@ public class QBTracker: NSObject {
     public static var shared: QBTracker = QBTracker()
     
     private var configurationManager: QBConfigurationManager?
+    private var lookupManager: QBLookupManager?
     private var sessionId: String?
     
     override private init() {
@@ -26,6 +27,9 @@ public class QBTracker: NSObject {
         QBLog.info("QBTracker Initalization...")
         
         configurationManager = QBConfigurationManager(with: id)
+        if let configurationManager = configurationManager {
+            lookupManager = QBLookupManager(withConfigurationManager: configurationManager, withTrakingId: id)
+        }
 //        sessionId = QBSessionManager.shared.getValidSessionId()
     }
     
