8e4c1a3ab3d660985a5903ff3fef7e4dcc87adfe
Dariusz Zajac <dariusz.zajac@miquido.com>
2017-08-31 11:39:23 +0200

    Code formatting: Use guard statement instead of force unwrapping when generating md5hash.

--- QBTracker/QubitSDK/Extensions/String+md5.swift
+++ QBTracker/QubitSDK/Extensions/String+md5.swift
@@ -11,12 +11,15 @@ import CommonCrypto
 
 extension String {
     var md5: String {
-        let str = self.cString(using: String.Encoding.utf8)
+        guard let str = self.cString(using: String.Encoding.utf8) else {
+            return self
+        }
+        
         let strLen = CC_LONG(self.lengthOfBytes(using: String.Encoding.utf8))
         let digestLen = Int(CC_MD5_DIGEST_LENGTH)
         let result = UnsafeMutablePointer<CUnsignedChar>.allocate(capacity: digestLen)
 
-        CC_MD5(str!, strLen, result)
+        CC_MD5(str, strLen, result)
 
         let hash = NSMutableString()
         for i in 0..<digestLen {
