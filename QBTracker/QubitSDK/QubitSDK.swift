167f7d4c48f7abbb0755f9b285b50168af8e3583
Jacek Grygiel <jacek.grygiel@gmail.com>
2017-09-18 22:47:20 +0200

    update qubitSDK old api

--- QBTracker/QubitSDK/QubitSDK.swift
+++ QBTracker/QubitSDK/QubitSDK.swift
@@ -113,22 +113,29 @@ public class QBTrackerInit: NSObject {
     
     static let sharedInstance: QBTrackerInit = QBTrackerInit()
     
+    @objc(applicationDidFinishLaunching)
+    @available(*, deprecated, message:"will be removed in next version")
     func applicationDidFinishLaunching() {
         
     }
     
+    @objc(applicationEnterForeground)
+    @available(*, deprecated, message:"will be removed in next version")
     func applicationEnterForeground() {
         
     }
-    
+    @objc(initConfig:completion:)
+    @available(*, deprecated, message:"will be removed in next version")
     func initConfig(fromForeground: Bool, completion: (() -> Void)?) {
         
     }
-    
+    @objc(invalidateConfigTimer)
+    @available(*, deprecated, message:"will be removed in next version")
     func invalidateConfigTimer() {
         
     }
-    
+    @objc(validateConfigTimer)
+    @available(*, deprecated, message:"will be removed in next version")
     func validateConfigTimer() {
         
     }
@@ -138,69 +145,83 @@ public class QBTrackerInit: NSObject {
 public class QBTrackerManager: NSObject {
     
     @objc
+    @available(*, deprecated, message:"will be removed in next version")
     public static let sharedManager: QBTrackerManager = QBTrackerManager()
 
     @objc(setTrackingId:)
+    @available(*, deprecated, message:"will be removed in next version")
     public func setTrackingId(trackingId: String) {
         
     }
     
     @objc(setDebugEndpoint:)
+    @available(*, deprecated, message:"will be removed in next version")
     public func setDebugEndpoint(endPointUrl: String) {
         
     }
     @objc(unsubscribeToTracking)
+    @available(*, deprecated, message:"will be removed in next version")
     public func unsubscribeToTracking() {
         
     }
     
     @objc(subscribeToTracking)
+    @available(*, deprecated, message:"will be removed in next version")
     public func subscribeToTracking() {
         
     }
     
     @objc(dispatchEvent:withData:)
+    @available(*, deprecated, message:"will be removed in next version")
     public func dispatchEvent(type: String, withData: [String: Any]) {
         
     }
     
     @objc(dispatchEvent:withStringData:)
+    @available(*, deprecated, message:"will be removed in next version")
     public func dispatchEvent(type: String, withStringData: String) {
         
     }
     
     @objc(dispatchSessionEvent:withEnd:)
+    @available(*, deprecated, message:"will be removed in next version")
     public func dispatchSessionEvent(startTimeStamp: TimeInterval, withEnd: TimeInterval) {
         
     }
     
     @objc(getUserID)
+    @available(*, deprecated, message:"will be removed in next version")
     public func getUserID() -> String {
         return ""
     }
     
     @objc(setStashInfo:key:withCallback:)
+    @available(*, deprecated, message:"will be removed in next version")
     public func setStashInfo(data: String, key: String, withCallback: (Int) -> Void) {
         
     }
     
     @objc(setStashInfo:withCallback:)
+    @available(*, deprecated, message:"will be removed in next version")
     public func setStashInfo(key: String, withCallback: (Int, String) -> Void) {
         
     }
     
     @objc(setStashInfoMultiple:withCallback:)
+    @available(*, deprecated, message:"will be removed in next version")
     public func setStashInfoMultiple(userkeys: [String], withCallback: (Int, [String: Any]) -> Void) {
         
     }
     
     @objc(getSegmentMembershipInfo:withCallback:)
+    @available(*, deprecated, message:"will be removed in next version")
     public func getSegmentMembershipInfo(userId: String, withCallback: (Int, [String]) -> Void) {
         
     }
     
 }
 
+@available(*, deprecated, message:"will be removed in next version")
 public let qubit: QBTrackerManager = QBTrackerManager()
 
 
