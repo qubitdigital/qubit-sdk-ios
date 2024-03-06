922d82080d47e6d2e63a2167412973c7ad175938
Henry Porter <henry@qubit.com>
2024-03-06 22:48:48 +0000

    Release 2.0.5 (#46)
    - Updated sessionEventCounter to include session and view events.
    - Implemented changes to QBEventType to ensure event counters are accurate.
    - Added further tests.

--- QBTracker/QubitSDK/QubitSDK.swift
+++ QBTracker/QubitSDK/QubitSDK.swift
@@ -11,7 +11,7 @@ import Foundation
 @objc
 public class QubitSDK: NSObject {
     /// Current QubitSDK version. Necessary since SPM doesn't use custom Info.plist.
-    public static let version = "2.0.4"
+    public static let version = "2.0.5"
     
     /// Return current tracking ID
     public static var trackingId: String = "Unknown. Call QubitSDK.start(...) before any other call."
